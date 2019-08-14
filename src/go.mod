898e6eff1cf0955cd50b4a98cb58e26808ff07a4
Ellery Zhang <allotory@msn.com>
2019-08-14 20:49:31 +0800

    feat: add benchmark test.

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,8 @@ go 1.12
 
 require (
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
+	github.com/segmentio/ksuid v1.0.2
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337
+	github.com/tietang/dbx v1.0.0
 )
--- src/test/over_po.go
+++ src/test/over_po.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+// 红包商品表，默认有符号类型字段
+type GoodsSigned struct {
+	Goods
+}
+
+// 红包商品表，无符号类型字段
+type GoodsUnsigned struct {
+	Goods
+}
+
 // 红包商品
 type Goods struct {
 	RemainAmount decimal.Decimal `db:"remain_amount"`
@@ -13,12 +23,12 @@ type Goods struct {
 	CreateAt time.Time `db:"created_at,omitempty"`
 	UpdatedAt time.Time `db:"updated_at,omitempty"`
 }
-
-// 红包商品表，无符号类型字段
-type GoodsUnsigned struct {
-	RemainAmount decimal.Decimal `db:"remain_amount"`
-	RemainQuantity int `db:"remain_quantity"`
-	EnvelopeNo string `db:"envelope_no,uni"`
-	CreateAt time.Time `db:"created_at,omitempty"`
-	UpdatedAt time.Time `db:"updated_at,omitempty"`
-}
+//
+//// 红包商品表，无符号类型字段
+//type GoodsUnsigned struct {
+//	RemainAmount decimal.Decimal `db:"remain_amount"`
+//	RemainQuantity int `db:"remain_quantity"`
+//	EnvelopeNo string `db:"envelope_no,uni"`
+//	CreateAt time.Time `db:"created_at,omitempty"`
+//	UpdatedAt time.Time `db:"updated_at,omitempty"`
+//}
